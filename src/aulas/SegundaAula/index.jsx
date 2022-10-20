11226792a5cf57d1210a149b5a389205aa2f4323
Vinicius Ocker Fagundes <60163731+ViniciusOcker@users.noreply.github.com>
2022-10-19 22:50:29 -0300

    Aula02

---
+++ src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,11 @@
+import { ListaComponente } from '../../components/ListaComponente';
+import './style.scss';
+
+export function SegundaAula(){
+    return (
+        <main>
+            <h1>Segunda Aula</h1>
+            <ListaComponente/>
+        </main>
+    )
+}
---
+++ src/components/ListaComponente/index.jsx
@@ -0,0 +1,20 @@
+export function ListaComponente() {
+    return(
+        <>
+            <h2>Projeto escolhido: Mercado Livre</h2>
+            <ul>
+                <li>
+                    <h3>Lista de produtos</h3>
+                    <p>um card com imagem, descrisão e preço do produto</p>
+                </li>
+                <li>
+                    <h3>Um grupo de componetes de produtos</h3>
+                    <p>uma lista de varios produtos</p>
+                </li>
+                <li>
+                    <h3>Uma lista de produtos partir de uma categoria</h3>
+                </li>
+            </ul>
+        </>
+    )
+}
